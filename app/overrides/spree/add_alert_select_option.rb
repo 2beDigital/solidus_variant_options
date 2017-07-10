8e5951d51e2e66b33810fd36356f8d6fa1d4b8d3
Noel <noel@2bedigital.com>
2017-07-10 09:13:41 +0100

    fix in override

--- app/overrides/spree/add_alert_select_option.rb
+++ app/overrides/spree/add_alert_select_option.rb
@@ -1,7 +1,7 @@
 Deface::Override.new(:virtual_path => "spree/products/show",
                      :name => "add_alert_select_option_to_show",
                      :insert_before => "erb[silent]:contains('cache')",
-                     :text => "<% content_for :backstrech do %>
+                     :text => "<% content_for :foot do %>
                                   <div id='shadow-alert'></div>
                                    <div id='alert-select-variant'>
                                     <p><i class='fa fa-exclamation-triangle fa-2x'></i></p>
